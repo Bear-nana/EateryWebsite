fe00c69ebda4de6ca489eef77452b800d42e8161
Bear-nana <32455326+Bear-nana@users.noreply.github.com>
2017-10-02 14:18:22 +0100

    Eatery Files
    HTML and CSS

---
+++ me.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html lang="eng">
+	<head>
+		<link rel="stylesheet" type="text/css" href="vendors/css/normalize.css">
+		<link rel="stylesheet" type="text/css" href="vendors/css/grid.css">
+		<link rel="stylesheet" type="text/css" href="vendors/css/2cols.css">
+		<link rel="stylesheet" type="text/css" href="vendors/css/3cols.css">
+		<link rel="stylesheet" type="text/css" href="vendors/css/col.css">
+		<link rel="stylesheet" type="text/css" href="vendors/css/ionicons.min.css">
+		<link rel="stylesheet" type="text/css" href="resources/css/style.css">
+		<title>PubFood</title>
+	</head>
+	
+	<body>
+		<header>
+			<nav>
+				<div class="row">
+					<img src="resources/css/img/logo.jpg" class="logo">
+					<ul class="main-nav">
+						<li><a href="#">Food delivery</a></li>
+						<li><a href="#">How it works</a></li>
+						<li><a href="#">Our cities</a></li>
+						<li><a href="#">Sign up</a></li>
+
+					</ul>
+				</div>
+			</nav>
+			<div class="hero-text-box">
+				<h1>GOODBYE JUNK FOOD.<br>HELLO SUPER HEALTHY MEALS.</h1>
+				<a class="btn btn-full" href="#">I'm hungry</a>
+				<a class="btn btn-ghost" href="#">Show me more</a>
+			</div>
+
+		</header>
+
+		<section class="section-features">
+			<div class="row">
+				<h2>Get Food Fast &mdash; Not Fast Food</h2>
+				<p class="long-copy">
+					Hello, we are PubFood, is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy!
+				</p>
+			</div>
+
+			<div class="row">
+				<div class="col span_1_of_4 box">
+					<i class="ion-ios-infinite-outline icon-big"></i>
+					<h3>Up to 365 days/year</h3>
+					<p>
+						It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+					</p>
+				</div>
+				<div class="col span_1_of_4 box">
+					<i class="ion-ios-stopwatch-outline icon-big"></i>
+					<h3>Ready in 20 mintues</h3>
+					<p>
+						Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.  
+					</p>
+				</div>
+				<div class="col span_1_of_4 box">
+					<i class="ion-ios-nutrition-outline icon-big"></i>
+					<h3>100% organic</h3>
+					<p>
+						The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from!
+					</p>
+				</div>
+				<div class="col span_1_of_4 box">
+					<i class="ion-ios-cart-outline icon-big"></i>	
+					<h3>Order anything</h3>
+					<p>
+						There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form!
+					</p>
+				</div>
+
+			</div>
+		</section>
+
+<section>
+	<div>
+		
+	</div>
+</section>
+		<section class="section-meals">
+			<ul class="meals-showcase clearfix">
+				<li>
+					<figure class="meal-photo">
+						<img src="resources/img/download1.jpg" >
+					</figure>
+				</li>
+				<li>
+					<figure class="meal-photo">
+						<img src="resources/img/download2.jpg" >
+					</figure>
+				</li>
+				<li>
+					<figure class="meal-photo">
+						<img src="resources/img/download3.jpg" >
+					</figure>
+				</li>
+				<li>
+					<figure class="meal-photo">
+						<img src="resources/img/download4.jpg" >
+					</figure>
+				</li>
+				
+			</ul>
+			<ul class="meals-showcase clearfix">
+				<li>
+					<figure class="meal-photo">
+						<img src="resources/img/download5.jpg" >
+					</figure>
+				</li>
+				<li>
+					<figure class="meal-photo">
+						<img src="resources/img/download6.jpg" >
+					</figure>
+				</li>
+				<li>
+					<figure class="meal-photo">
+						<img src="resources/img/download7.jpg" >
+					</figure>
+				</li>
+				<li>
+					<figure class="meal-photo">
+						<img src="resources/img/download8.jpg">
+					</figure>
+				</li>
+				
+			</ul>
+		</section>
+  <section>
+  	<div><br><br></div>
+  </section>
+		<section class="section-steps">
+			<div class="row">
+				<h2 class="how-it-works">How it works &mdash; Simple as 1, 2, 3</h2>
+			</div>
+			<div class="row">
+				<div class="col span_1_of_2 steps-box">
+					<img src="resources/img/download.jpg" class="app-screen">	
+				</div>
+				<div class="col span_1_of_2 steps-box">
+					<div class="works-step">
+						<div>1</div>
+						<p>Choose the subscription plan that best fits your needs and sign up today.</p>
+					</div>
+					<div class="works-step">
+						<div>2</div>
+						<p>Order your delicious meal using our mobile app or website. Or you can even call us!</p>
+					</div>
+					<div class="works-step">
+						<div>3</div>
+						<p>Enjoy your meal after less than 20 minutes. See you the next time!</p>
+					</div>
+
+					<a href="#" class="btn-app"><img src="resources/img/App store.jpg"></a>
+					<a href="#" class="btn-app"><img src="resources/img/google.jpg"></a>
+
+				</div>
+			</div>
+		</section>
+		
+		<section class="section-cities">
+			<div class="row">
+				<h2>We're currently in these cities</h2>
+			</div>
+			<div class="row">
+				<div class="col span_1_of_4 box">
+					<img src="resources/img/lisbon.jpg">
+					<h3>Lisbon</h3>
+					<div class="city-feature">
+						<i class="ion-ios-person icon-small"></i>
+						1600+ happy eaters
+					</div>
+					<div class="city-feature">
+						<i class="ion-ios-star icon-small"></i>
+						60+ top chefs
+					</div>
+					<div class="city-feature">
+						<i class="ion-social-twitter icon-small"></i>
+						<a href="#">@pubfood_lx</a>
+					</div>
+				</div>
+				<div class="col span_1_of_4 box">
+					<img src="resources/img/francisco.jpg">
+					<h3>San Francisco</h3>
+					<div class="city-feature">
+						<i class="ion-ios-person icon-small"></i>
+						3700+ happy eaters
+					</div>
+					<div class="city-feature">
+						<i class="ion-ios-star icon-small"></i>
+						160+ top chefs
+					</div>
+					<div class="city-feature">
+						<i class="ion-social-twitter icon-small"></i>
+						<a href="#">@pubfood_sf</a>
+					</div>
+				</div>
+				<div class="col span_1_of_4 box">
+					<img src="resources/img/berlin.jpg">
+					<h3>Berlin</h3>
+					<div class="city-feature">
+						<i class="ion-ios-person icon-small"></i>
+						2300+ happy eaters
+					</div>
+					<div class="city-feature">
+						<i class="ion-ios-star icon-small"></i>
+						110+ top chefs
+					</div>
+					<div class="city-feature">
+						<i class="ion-social-twitter icon-small"></i>
+						<a href="#">@pubfood_berlin</a>
+					</div>
+				</div>
+				<div class="col span_1_of_4 box">
+					<img src="resources/img/london.jpg">
+					<h3>London</h3>
+					<div class="city-feature">
+						<i class="ion-ios-person icon-small"></i>
+						1200+ happy eaters
+					</div>
+					<div class="city-feature">
+						<i class="ion-ios-star icon-small"></i>
+						50+ top chefs
+					</div>
+					<div class="city-feature">
+						<i class="ion-social-twitter icon-small"></i>
+						<a href="#">@pubfood_london</a>
+					</div>
+				</div>
+
+			</div>
+		</section>
+		<section><div><br><br></div></section>
+
+		<section class="section-testimonials">
+			<div class="row">
+				<h2>Our customers can't live without us</h2>
+			</div>
+			<div class="row">
+				<div class="col span_1_of_3">
+					<blockquote>
+						<em>PubFood is awesome! I just lauched a startup which leaves me with no time for cooking, so PubFood is a life-saver. Now that i got used to it, i couldn't live without my daily meals!</em>
+						<cite><img src="resources/img/Koala.jpg">Shola Daniel</cite>
+					</blockquote>
+				</div>
+				<div class="col span_1_of_3">
+					<blockquote>
+						<em>PubFood is awesome! I just lauched a startup which leaves me with no time for cooking, so PubFood is a life-saver. Now that i got used to it, i couldn't live without my daily meals!</em>
+						<cite><img src="resources/img/Chrysanthemum.jpg">Cherry Blossom</cite>
+					</blockquote>
+				</div>
+				<div class="col span_1_of_3">
+					<blockquote>
+						<em>PubFood is awesome! I just lauched a startup which leaves me with no time for cooking, so PubFood is a life-saver. Now that i got used to it, i couldn't live without my daily meals!</em>
+						<cite><img src="resources/img/Penguins.jpg">Pen Dan</cite>
+					</blockquote>
+				</div>
+			</div>
+
+		</section>
+
+		<section class="section-plans">
+			<div class="row">
+				<h2>Start eating healthy today</h2>
+			</div>
+			<div class="row">
+				<div class="col span_1_of_3">
+					<div class="plan-box">
+						<div>
+							<h3>Premium</h3>
+							<p class="plan-price">$3</p>
+						</div>
+						<div>
+							
+						</div>
+						<div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</section>
+
+
+
+
+	</body>
+</html>
+
